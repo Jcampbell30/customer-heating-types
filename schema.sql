330cc7d15dda3a741f19832dffdf8d8193d020dd
Kain McDonald <sikeddemon@gmail.com>
2023-09-21 17:12:59 -0400

    <fix>: Updated schema to reduce redundant data.

--- schema.sql
+++ schema.sql
@@ -1,9 +1,14 @@
 CREATE DATABASE output_db;
 
-CREATE TABLE output_data (
+CREATE TABLE power_data (
     premise_id INT,
     data_date VARCHAR(50),
-    weather_data FLOAT,
     power_data FLOAT,
     PRIMARY KEY (premise_id, data_date)
+);
+
+CREATE TABLE weather(
+    data_date VARCHAR(50),
+    temp FLOAT,
+    PRIMARY KEY (data_date)
 );
