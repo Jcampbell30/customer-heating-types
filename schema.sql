e7e447ac6ed9bb99d78e9b143edc52edbdf37967
Kain McDonald <sikeddemon@gmail.com>
2023-10-01 17:28:12 -0400

    <fix>: Fixed the layout of the output_db schema

--- schema.sql
+++ schema.sql
@@ -1,20 +1,21 @@
 CREATE DATABASE output_db;
 
+CREATE TABLE weather(
+    data_date VARCHAR(50),
+    temp FLOAT,
+    PRIMARY KEY (data_date)
+);
+
 CREATE TABLE power_data (
     premise_id INT,
     data_date VARCHAR(50),
     power_data FLOAT,
+    FOREIGN KEY (data_date) REFERENCES weather(data_date),
     PRIMARY KEY (premise_id, data_date)
 );
 
 CREATE TABLE predictions (
     premise_id INT,
     correlation_coefficient FLOAT,
-    FOREIGN KEY (premise_id) REFERENCES (power_data(premise_id))
-);
-
-CREATE TABLE weather(
-    data_date VARCHAR(50),
-    temp FLOAT,
-    FOREIGN KEY (data_date) REFERENCES (power_data(data_date))
+    FOREIGN KEY (premise_id) REFERENCES power_data(premise_id)
 );
