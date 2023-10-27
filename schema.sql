41f2cf30c76eac8220e532c214b827ae907725ef
Amber <31871204+AmbJShimel@users.noreply.github.com>
2023-10-27 16:27:11 -0400

    <new> Added anomaly_types table
    Introduced anomaly_types table to better coincide with object oriented principles, better control data consistency, and generally be easier to work with.

--- schema.sql
+++ schema.sql
@@ -18,8 +18,15 @@ CREATE TABLE predictions (
 CREATE TABLE anomalies (
     premise_id INT,
     data_date VARCHAR(50),
+    anomaly_id INT,
+    FOREIGN KEY (premise_id) REFERENCES power_data(premise_id),
+    FOREIGN KEY (data_date) REFERENCES power_data(data_date),
+    FOREIGN KEY (anomaly_id) REFERENCES anomaly_types(anomaly_id)
+);
+
+CREATE TABLE anomaly_types (
+    anomaly_id INT,
     anomaly_type varchar(50),
     anomaly_desc varchar(50),
-    FOREIGN KEY (premise_id) REFERENCES power_data(premise_id),
-    FOREIGN KEY (data_date) REFERENCES power_data(data_date)
+    PRIMARY KEY (anomaly_id)
 );
