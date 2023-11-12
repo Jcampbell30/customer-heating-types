65c0a527577a57e1133369b24eeddebb1fab0519
Kain McDonald <sikeddemon@gmail.com>
2023-11-12 18:43:46 -0500

    <fix>: Corrected schema ordering.

--- schema.sql
+++ schema.sql
@@ -15,6 +15,13 @@ CREATE TABLE predictions (
     FOREIGN KEY (premise_id) REFERENCES power_data(premise_id)
 );
 
+CREATE TABLE anomaly_types (
+    anomaly_type_id INT,
+    anomaly_type varchar(50),
+    anomaly_desc varchar(50),
+    PRIMARY KEY (anomaly_type_id)
+);
+
 CREATE TABLE anomalies (
     id INT NOT NULL AUTO_INCREMENT,
     premise_id INT,
@@ -24,11 +31,4 @@ CREATE TABLE anomalies (
     FOREIGN KEY (premise_id) REFERENCES power_data(premise_id),
     FOREIGN KEY (data_date) REFERENCES power_data(data_date),
     FOREIGN KEY (anomaly_type_id) REFERENCES anomaly_types(anomaly_type_id)
-);
-
-CREATE TABLE anomaly_types (
-    anomaly_type_id INT,
-    anomaly_type varchar(50),
-    anomaly_desc varchar(50),
-    PRIMARY KEY (anomaly_type_id)
 );
